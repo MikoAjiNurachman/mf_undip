3d6927e294e7ed6155dea8d8dbf416d4fc3c6911
MikoAjiNurachman <mikoajin14@gmail.com>
2019-08-26 15:41:35 +0700

    database

--- application/config/database.php
+++ application/config/database.php
@@ -72,14 +72,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $active_group = 'default';
 $query_builder = TRUE;
-$url = parse_url(getenv("Heroku_Postgres_url"));
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => $url["host"],
-	'username' => $url["user"],
-	'password' => $url["pass"],
-	'database' => substr($url["path"], 1),
+	'hostname' => 'localhost',
+	'username' => 'root',
+	'password' => '',
+	'database' => 'mf_undip',
 	'dbdriver' => 'postgre',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
